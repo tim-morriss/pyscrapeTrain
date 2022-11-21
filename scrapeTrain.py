ad4e70474f33be6318630092aa845a1741aaec86
Tim <tm224@hw.ac.uk>
2022-11-21 12:43:54 +0000

    Try and download hidden tracks

--- scrapeTrain.py
+++ scrapeTrain.py
@@ -31,15 +31,30 @@ def slugify(value, allow_unicode=False):
     return re.sub(r'[-\s]+', ' ', value).strip('-_')
 
 
-# os agnostic home path
-home_path = str(Path.home())
-
 # cli argument for TrakTrain URL
 parser = argparse.ArgumentParser(description="Script for downloading traktrain tracks.")
-parser.add_argument('url', type=str, help="url for the traktrain profile")
+parser.add_argument(
+    'url',
+    type=str,
+    help="url for the traktrain profile")
+parser.add_argument(
+    '-d',
+    '--dir',
+    dest='directory',
+    default=None,
+    type=str,
+    help="directory to save mp3s to. format: <dir>/pyscrapeTrain/<artist>"
+)
 args = parser.parse_args()
 tt_url = args.url
 artist = tt_url.rsplit('/', 1)[-1]  # artist name from end of url
+if not args.directory:
+    # os agnostic home path
+    output_dir = str(Path.home())
+else:
+    output_dir = args.directory
+# define where to save mp3s
+dir_path = f"{output_dir}/pyscrapeTrain/{artist}"
 
 # get TrakTrain page as beautiful soup object
 r = requests.get(tt_url)
@@ -49,7 +64,10 @@ mp3_urls = []
 artwork = []
 
 # find all tracks on the page to get title and mp3-url
-for d in soup.find_all("div", {"class": "js-profile-track"}):
+for d in soup.find_all(
+        "div",
+        {"class": ["js-profile-track", "js-profile-track hidden"]}
+):
     # srcset get 2x value, example srcset:
     # srcset="https://d369yr65ludl8k.cloudfront.net/60x60/138296/5d3828d2-7c7e-4d7f-b515-4dd97c83edc9.jpg 1x,
     # https://d369yr65ludl8k.cloudfront.net/120x120/138296/5d3828d2-7c7e-4d7f-b515-4dd97c83edc9.jpg 2x"
@@ -71,35 +89,46 @@ for d in soup.find_all("div", {"class": "js-profile-track"}):
 # print(mp3_urls)
 # print(artwork)
 
-url_stub = ""
+# url_stub = ""
+url_stubs = []
 
 # find url stub for mp3s
-scripts = soup.find_all("script", {"type": "text/javascript"})
+scripts = soup.find_all("script")
 for script in scripts:
-    # print(stub)
-    if "var AWS_BASE_URL" in script.text:
+    if "AWS" in script.text:
         s = script.text
-        url_stub = re.search("(?P<url>https?://[^\s]+)\'", s).group("url")
-
-# define where to save mp3s
-dir_path = f"{home_path}/pyscrapeTrain/{artist}"
+        # url_stub = re.search("(?P<url>https?://[^\s]+)\'", s).group("url")
+        url_stubs.append(re.search("(?P<url>https?://[^\s]+)\'", s).group("url"))
 
 # make dir if not exists
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
+
+def test_urls(urls, track_name):
+    for url in urls:
+        try:
+            req = Request(url)
+            req.add_header('Origin', 'https://traktrain.com')
+            req.add_header("Referer", "https://traktrain.com/")
+            return urlopen(req).read()
+        except HTTPError as e:
+            print(f'• Track {track_name} with error {e}, continuing...')
+            continue
+
+
 # use stub and mp3_urls to find all mp3 files on AWS and download them
 # avoid stop stealing beats page by adding in origin and referer to request headers
 print(f"Downloading {len(mp3_urls)} tracks by {artist}:")
 for i in tqdm(range(len(mp3_urls))):
-    url = url_stub + mp3_urls[i]
-    req = Request(url)
-    req.add_header('Origin', 'https://traktrain.com')
-    req.add_header("Referer", "https://traktrain.com/")
-    try:
-        content = urlopen(req).read()
+    path = f"{dir_path}/{track_names[i]}.mp3"
+    if os.path.exists(path):
+        print(f"• {path} already exists, skipping...")
+        continue
+    urls = [url_stub + mp3_urls[i] for url_stub in url_stubs]
+    content = test_urls(urls, track_names[i])
+    if content:
         mp3 = MP3(BytesIO(content))
-        path = f"{dir_path}/{track_names[i]}.mp3"
         if mp3.tags is None:
             mp3.tags = ID3()
         # ID3 Frames:
@@ -118,8 +147,5 @@ for i in tqdm(range(len(mp3_urls))):
         with open(path, 'wb') as f:
             f.write(content)
         mp3.save(path)
-    except HTTPError as e:
-        # sometimes tracks 404 on AWS
-        # this is a problem with TrakTrain not the script
-        print(f"{e} \nfor track '{track_names[i]}' with url: {url}, continuing....")
+    else:
         continue
