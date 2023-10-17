a60d3f4e39176424f4b01ec19637e451336369f2
Tim <timothy.morriss@natwest.com>
2023-10-17 15:01:56 +0100

    Fixed issue where script only downloaded first page

--- helpers.py
+++ helpers.py
@@ -1,9 +1,30 @@
 import os
 import re
+import requests
 import unicodedata
-from urllib.error import HTTPError
+from typing import List
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from urllib.error import HTTPError
 from urllib.request import Request, urlopen
+
+
+class TrakTrain:
+    def __init__(
+            self,
+            artist: str,
+            artwork: List[str],
+            track_names: List[str],
+            mp3_urls: List[str],
+            soup: BeautifulSoup,
+            dir_path: str
+    ):
+        self.artist = artist
+        self.artwork = artwork
+        self.track_names = track_names
+        self.mp3_urls = mp3_urls
+        self.soup = soup
+        self.dir_path = dir_path
 
 
 def is_local(url: str):
@@ -18,6 +39,7 @@ def is_local(url: str):
     if url_parsed.scheme in ('file', ''):  # Possibly a local file
         return os.path.exists(url_parsed.path)
     return False
+
 
 def slugify(value: str, allow_unicode=False):
     """
@@ -98,3 +120,86 @@ def is_tt_url(tt_url: str):
         return True
     else:
         return False
+
+
+def get_data_endpoint(soup: BeautifulSoup, artist: str):
+    """
+    Function to find data endpoint from tt page.
+    Data endpoint is where tt pulls the track information for artists.
+
+    :param soup:
+        tt beautifulsoup object
+    :return:
+        string of the data-endpoint
+    """
+    data_endpoint = soup.find(
+        "form",
+        {"class": 'js-filter-form'}
+    )
+    if data_endpoint.attrs.get('data-endpoint', False):
+        return data_endpoint.attrs['data-endpoint'].split('/')[-1]
+    else:
+        raise Exception(f"{artist} doesn't seem to have any tracks...")
+
+
+def compile_page(data_endpoint: str):
+    stop = True
+    i = 0
+    track_names = []
+    mp3_urls = []
+    artwork = []
+
+    while stop:
+        i += 1
+        url = f"https://traktrain.com/profile-tracks/{data_endpoint}?search=&sort=latest&page={i}"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.json()['content'], 'html.parser')
+        if soup.find("div", {"class": "empty-search"}):
+            stop = False
+        else:
+            for d in soup.find_all(
+                "div",
+                {"class": ["js-profile-track", "js-profile-track hidden"]}
+            ):
+                artwork.append(
+                    d.find(
+                        "img",
+                        attrs={"src": True}
+                    )['src'].replace('60x60', '360x360')
+                )
+                track_names.append(
+                    slugify(d.find(
+                        "div",
+                        {"class": "title__name-tooltip"}
+                    ).text.strip('\n'), allow_unicode=True)
+                )
+                mp3_urls.append(d.find(attrs={"data-id": True})['data-id'])
+
+    return track_names, mp3_urls, artwork
+
+
+def get_tracks(tt_url: str, output_dir: str):
+    """
+    Returns TrakTrain object of given traktrain profile.
+
+    :param tt_url: str
+        URL for TrakTrain profile
+    :param output_dir: str
+        Output directory for the files
+    :return:
+        Returns a TrakTrain Object
+    """
+    if not is_tt_url(tt_url):
+        raise Exception("Doesn't look like a TrakTrain.com url...")
+    r = requests.get(tt_url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    artist = slugify(
+        soup.find("h1", {"class": "profile-bio__name"}).text
+    ).strip()
+    dir_path = f"{output_dir}/{artist}"
+
+    data_endpoint = get_data_endpoint(soup, artist)
+    track_names, mp3_urls, artwork = compile_page(data_endpoint)
+
+    return TrakTrain(artist, artwork, track_names, mp3_urls, soup, dir_path)
--- scrapeTrain.py
+++ scrapeTrain.py
@@ -1,19 +1,19 @@
-import re
 import os
-from io import BytesIO
-import requests
+import re
 import argparse
-from tqdm import tqdm
+import requests
+from halo import Halo
+from io import BytesIO
 from pathlib import Path
-from bs4 import BeautifulSoup
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, TPE1, TIT2, TALB, APIC
+from simple_chalk import chalk
 from urllib.error import HTTPError
 from urllib.request import urlopen
-from helpers import slugify, is_local, try_artwork, test_urls, is_tt_url
+from mutagen.id3 import ID3, TPE1, TIT2, TALB, APIC
+from helpers import slugify, is_local, try_artwork, test_urls, get_tracks
 
 
-def scrape_train(tt_url, output_dir):
+def scrape_train(tt_url, output_dir, overwrite):
     """
     Downloads tracks from TrakTrain.com page to output_dir/<artist> name.
 
@@ -21,38 +21,10 @@ def scrape_train(tt_url, output_dir):
         str: url of TrakTrain.com page
     :param output_dir:
         str: path to output directory
+    :param overwrite:
+        bool: whether to overwrite tracks if exist
     """
-    # get TrakTrain page as beautiful soup object
-    if not is_tt_url(tt_url):
-        raise Exception("Doesn't look like a TrakTrain.com url...")
-    r = requests.get(tt_url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    track_names = []
-    mp3_urls = []
-    artwork = []
-    artist = slugify(
-        soup.find("h1", {"class": "profile-bio__name"}).text
-    ).strip()
-    dir_path = f"{output_dir}/{artist}"
-
-    # find all tracks on the page to get title and mp3-url
-    for d in soup.find_all(
-            "div",
-            {"class": ["js-profile-track", "js-profile-track hidden"]}
-    ):
-        artwork.append(
-            d.find(
-                "img",
-                attrs={"src": True}
-            )['src'].replace('60x60', '360x360')
-        )
-        track_names.append(
-            slugify(d.find(
-                "div",
-                {"class": "title__name-tooltip"}
-            ).text.strip('\n'), allow_unicode=True)
-        )
-        mp3_urls.append(d.find(attrs={"data-id": True})['data-id'])
+    tt = get_tracks(tt_url, output_dir)
 
     # print(track_names)
     # print(mp3_urls)
@@ -61,7 +33,7 @@ def scrape_train(tt_url, output_dir):
     url_stubs = []
 
     # find url stub for mp3s
-    scripts = soup.find_all("script")
+    scripts = tt.soup.find_all("script")
     for script in scripts:
         if "AWS" in script.text:
             # print(script)
@@ -70,49 +42,65 @@ def scrape_train(tt_url, output_dir):
                 re.search("(?P<url>https?://[^\s]+)\'", s).group("url"))
 
     # make dir if not exists
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+    if not os.path.exists(tt.dir_path):
+        os.makedirs(tt.dir_path)
 
     # use stub and mp3_urls to find all mp3 files on AWS and download them
     # avoid stop stealing beats page by adding in origin and referer to
     # request headers
-    print(f"Downloading {len(mp3_urls)} tracks by {artist}:")
-    for i in tqdm(range(len(mp3_urls))):
-        path = f"{dir_path}/{track_names[i]}.mp3"
-        if os.path.exists(path):
-            print(f"• {path} already exists, skipping...")
-            continue
-        urls = [url_stub + mp3_urls[i] for url_stub in url_stubs]
-        content = test_urls(urls)
-        if content:
-            mp3 = MP3(BytesIO(content))
-            if mp3.tags is None:
-                mp3.tags = ID3()
-            # ID3 Frames:
-            # https://mutagen.readthedocs.io/en/latest/api/id3_frames.html
-            # #id3v2-3-4-frames
-            mp3.tags['TPE1'] = TPE1(encoding=3, text=artist)
-            mp3.tags['TIT2'] = TIT2(encoding=3, text=track_names[i])
-            try:
-                album_art = urlopen(artwork[i]).read()
-            except HTTPError:
-                album_art = try_artwork(artwork[i])
-            mp3.tags['APIC'] = APIC(
-                encoding=3,
-                mime='image/jpeg',
-                type=3,
-                desc=u'Cover',
-                data=album_art
-            )
-            if ALBUM:
-                mp3.tags['TALB'] = TALB(encoding=3, text=ALBUM)
-            # Save mp3 then save metadata
-            with open(path, 'wb') as f:
-                f.write(content)
-            mp3.save(path)
-        else:
-            print(f"Traktrain error skipping {track_names[i]}")
-            continue
+    print(f"Downloading {len(tt.mp3_urls)} tracks by {tt.artist}:")
+    for i in range(len(tt.mp3_urls)):
+        num = i + 1
+        with Halo(
+            text=f"{chalk.magenta(f'Downloading track {num}: {tt.track_names[i]}...')}",
+            spinner="dots"
+        ) as halo:
+            path = f"{tt.dir_path}/{tt.track_names[i]}.mp3"
+            if os.path.exists(path) and not overwrite:
+                # print(f"• {path} already exists, skipping...")
+                halo.stop_and_persist(
+                    symbol=str(f'{chalk.red("✖")}'),
+                    text=f'{chalk.red.dim(f"{num} • {path} already exists, skipping...")}'
+                )
+                continue
+            urls = [url_stub + tt.mp3_urls[i] for url_stub in url_stubs]
+            content = test_urls(urls)
+            if content:
+                mp3 = MP3(BytesIO(content))
+                if mp3.tags is None:
+                    mp3.tags = ID3()
+                # ID3 Frames:
+                # https://mutagen.readthedocs.io/en/latest/api/id3_frames.html
+                # #id3v2-3-4-frames
+                mp3.tags['TPE1'] = TPE1(encoding=3, text=tt.artist)
+                mp3.tags['TIT2'] = TIT2(encoding=3, text=tt.track_names[i])
+                try:
+                    album_art = urlopen(tt.artwork[i]).read()
+                except HTTPError:
+                    album_art = try_artwork(tt.artwork[i])
+                mp3.tags['APIC'] = APIC(
+                    encoding=3,
+                    mime='image/jpeg',
+                    type=3,
+                    desc=u'Cover',
+                    data=album_art
+                )
+                if ALBUM:
+                    mp3.tags['TALB'] = TALB(encoding=3, text=ALBUM)
+                # Save mp3 then save metadata
+                with open(path, 'wb') as f:
+                    f.write(content)
+                mp3.save(path)
+                halo.stop_and_persist(
+                    symbol=f'{chalk.green("✔")}',
+                    text=chalk.green.dim(f"{num} Saved {chalk.white.bold(tt.track_names[i])} {chalk.green.dim(path)}")
+                )
+            else:
+                halo.stop_and_persist(
+                    symbol=str(f'{chalk.red("✖")}'),
+                    text=f"{chalk.red.dim(f'{num} Traktrain error skipping {tt.track_names[i]}')}"
+                )
+                continue
 
 
 if __name__ == '__main__':
@@ -140,6 +128,13 @@ if __name__ == '__main__':
         type=str,
         help="Custom name for ID3 tags, for sorting"
     )
+    parser.add_argument(
+        '-o',
+        '--overwrite',
+        dest='overwrite',
+        default=False,
+        action="store_true"
+    )
 
     args = parser.parse_args()
     if not args.directory:
@@ -149,6 +144,7 @@ if __name__ == '__main__':
         output_dir = args.directory
     # define where to save mp3s
     ALBUM = args.album
+    overwrite = args.overwrite
 
     if is_local(args.url):
         try:
@@ -156,11 +152,14 @@ if __name__ == '__main__':
                 with open(args.url) as f:
                     lines = f.readlines()
                     for line in lines:
-                        scrape_train(line.strip(), output_dir)
+                        try:
+                            scrape_train(line.strip(), output_dir, overwrite)
+                        except Exception as e:
+                            print(e)
             else:
                 raise Exception("Please supply a txt file")
         except Exception as e:
             print(e)
     else:
         tt_url = args.url
-        scrape_train(tt_url, output_dir)
+        scrape_train(tt_url, output_dir, overwrite)
